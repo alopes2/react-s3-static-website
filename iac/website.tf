1f852be8830b2572fef6ebcf264ba645bcd42ded
Andre <andrevitorlopes@gmail.com>
2024-05-17 22:09:20 +0200

    Add initial website

---
+++ iac/website.tf
@@ -0,0 +1,41 @@
+resource "aws_s3_bucket" "website" {
+  bucket = "YOUR_BUCKET_NAME"
+}
+
+resource "aws_s3_bucket_public_access_block" "website_bucket_public_access" {
+  bucket                  = aws_s3_bucket.website.id
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "public_bucket_policy" {
+  bucket = aws_s3_bucket.website.id
+  policy = data.aws_iam_policy_document.bucket_policy.json
+}
+
+resource "aws_s3_bucket_website_configuration" "website_configuration" {
+  bucket = aws_s3_bucket.website.id
+  index_document {
+    suffix = "index.html"
+  }
+  error_document {
+    key = "index.html"
+  }
+}
+
+data "aws_iam_policy_document" "bucket_policy" {
+  statement {
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+    actions = [
+      "s3:GetObject"
+    ]
+    resources = [
+      "arn:aws:s3:::${aws_s3_bucket.website.bucket}/*"
+    ]
+  }
+}
